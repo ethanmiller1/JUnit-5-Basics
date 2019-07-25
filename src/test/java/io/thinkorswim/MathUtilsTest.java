f1a810a10cfbe1b69febf10e6900bb1abf2abfee
Ethan Miller <ethan.romans5.8@gmail.com>
2019-07-25 14:47:21 -0500

    add circle area method

--- src/test/java/io/thinkorswim/MathUtilsTest.java
+++ src/test/java/io/thinkorswim/MathUtilsTest.java
@@ -7,11 +7,16 @@ import static org.junit.jupiter.api.Assertions.*;
 class MathUtilsTest {
 
     @Test
-    void test() {
+    void add() {
         MathUtils mathUtils = new MathUtils();
         int expected = 1;
         int actual = mathUtils.add(0, 1);
-        assertEquals(expected, actual);
+        assertEquals(expected, actual, "The add method should add two numbers");
     }
 
+    @Test
+    void computeCircleArea() {
+        MathUtils mathUtils = new MathUtils();
+        assertEquals(314.1592653589793, mathUtils.computeCircleArea(10), "Should return right circle area");
+    }
 }
